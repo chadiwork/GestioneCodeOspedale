1f109383223790274261519cd08730537a4237cb
Chado <hardachadi@gmail.com>
2016-02-04 09:19:28 +0100

    0.0.1 aggiunto adt

---
+++ src/adt/Coda.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package adt;
+
+/**
+ *
+ * Esempio semplificato di coda, realizzata come lista semplice
+ * formata da nodi legati tra loro da un riferimento
+ * Il nodo ha una struttura fissa e contiene solo una stringa
+ * @author Cenacchi
+ */
+public class Coda {
+    private nodo fronte,ultimo;
+    public Coda(){
+        fronte=null;
+    }
+    public boolean isEmpty(){
+        return fronte==null;
+    }
+
+    public nodo getFronte() {
+        return fronte;
+    }
+
+    public nodo getUltimo() {
+        return ultimo;
+    }
+    
+    public void deQueue() throws Exception{
+        //si estrae dalla cima (fronte)
+        if (this.isEmpty()) throw new Exception ("impossibile effettuare dequeue perchè la coda è vuota");
+        fronte=fronte.getNext();
+    }
+    public void enQueue(String info){
+        //è un append alla fine della coda
+        nodo n=new nodo();
+        n.setInfo(info);
+        if (isEmpty())
+            ultimo=fronte=n;
+        else {
+            ultimo.setNext(n);
+            ultimo=n;
+            
+        }
+
+
+    }
+
+
+}
---
+++ src/adt/nodo.java
@@ -0,0 +1,90 @@
+package adt;
+/**
+ * Esempio semplificato di nodo per lista semplice concatenata
+ * formata da nodi legati tra loro da un riferimento
+ * Contiene solo una stringa
+ * Implementa l'interfaccia Comparable anche se nel caso specifico
+ * non sarebbe indispensabili
+ * @author peroni
+ */
+public class nodo implements Comparable{
+    /**
+     * Rappresenta una versione semplificate dell'elemento contenuto nella lista
+     */
+    private String info;
+    /**
+     * Riferimento all'elemento successivo
+     */
+    private nodo next;
+    /**
+     * Costruttore di default inizializza a null sia l'informazione che il
+     * puntatore all'elemento successivo
+     */
+    public nodo(){
+        info="";
+        next=null;
+    }
+    /**
+     * Costruttore parametrico inizializza l'informazione e il riferimento all'
+     * elemento successivo in base ai parametri ricevuti in ingresso
+     * @param info Rappresentazione semplificata dell'informazione
+     * @param next riferimento all'elemento successivo (null per l'ultimo)
+     */
+    public nodo(String info,nodo next){
+        this.info=info;
+        this.next=next;
+    }
+    /**
+     * Restituisce l'elemento di informazione contenuto nel nodo
+     * @return informazione contenuta nel nodo
+     */
+    public String getInfo(){
+        return info;
+    }
+    /**
+     * Restituisce il riferimento al nodo successivo
+     * @return riferimento al nodo successivo (null se è l'ultimo)
+     */
+    public nodo getNext(){
+        return next;
+    }
+    /**
+     * Modifica l'informazione in base al parametro di ingresso
+     * @param info informazione da inserire nel nodo
+     */
+    public void setInfo(String info){
+        this.info=info;
+    }
+    /**
+     * Modifica il riferimento al nodo successivo in base al parametro di ingresso
+     * @param next riferimento al nodo successivo
+     */
+    public void setNext(nodo next){
+        this.next=next;
+    }
+    /**
+     * scavalca il metodo di default restituendo una rappresentazione
+     * testuale del nodo
+     * @return rappresentazione testuale del nodo
+     */
+    @Override
+    public String toString(){
+        return info;
+    }
+    /**
+     * Implementa il metodo compareTo dell'interfaccia Comparable
+     * confronta due nodi secondo un criterio specifico relativo
+     * all'informazione contenuta
+     * Nel caso semplificato il confronto è banale perchè il nodo contiene
+     * solo una stringa
+     * @param o riferimento all'oggetto da confrontare
+     * @return 1 se questo oggetto è maggiore dell'oggetto da confrontare
+     * 0 se sono uguali, -1 se l'oggetto è minore
+     */
+    public int compareTo(Object o) {
+        int ris=0;
+        nodo n=(nodo) o;
+        ris=this.getInfo().compareTo(n.getInfo());
+        return ris;
+    }
+}
